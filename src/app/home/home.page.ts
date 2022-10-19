3cab8876f803e32904f2d1d0b0dbfeec21bb7e41
Sidi <sidi.mohamed.taleb@cashflowpositif.fr>
2022-10-19 10:36:34 +0200

    Initial commit

---
+++ src/app/home/home.page.ts
@@ -0,0 +1,57 @@
+import { Component } from '@angular/core';
+
+
+
+@Component({
+  selector: 'app-home',
+  templateUrl: 'home.page.html',
+  styleUrls: ['home.page.scss'],
+})
+export class HomePage {
+
+  links = [
+    {
+    link:"",
+    text:"Produits et Promotions",
+    img:""
+    }, 
+    {
+      link:"",
+      text:"Produits et Promotions",
+      img:""
+    }, 
+    {
+      link:"",
+      text:"Bateaux",
+      img:""
+    }, 
+    {
+      link:"",
+      text:"Restaurants",
+      img:""
+    }, 
+
+    {
+      link:"",
+      text:"Recettes",
+      img:""
+    },
+    {
+      link:"",
+      text:"Contact",
+      img:""
+    } 
+    
+
+
+  ]
+  
+  
+
+  
+
+  constructor() {}
+
+  
+
+}
